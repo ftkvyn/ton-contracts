7b9658efcc75472a2e0060aeff4b187775a4a4a2
ftkvyn <ftkvyn@gmail.com>
2019-10-08 18:50:44 +0200

    Second test, cleanup

--- work/multisig/build.sh
+++ work/multisig/build.sh
@@ -1,9 +1,16 @@
 rm ./dist/*.*
 func -AP -O 0 -o ./dist/new-multisig-programm.fif ./lib/stdlib.fc ./src/multisig.fc
 cat ./dist/new-multisig-programm.fif ./src/new-multisig-wallet_script.fif > ./dist/new-multisig.fif
+
 cat ./dist/new-multisig-programm.fif ./src/new-multisig-wallet_script.fif ./test/test-1_script.fif > ./temp/test-1.fif
 sed -i 's/recv_external/main/' ./temp/test-1.fif
 sed -i 's/recv_internal/recv_internal_deleted/' ./temp/test-1.fif
+
+cat ./dist/new-multisig-programm.fif ./src/new-multisig-wallet_script.fif ./test/test-2_script.fif > ./temp/test-2.fif
+sed -i 's/recv_external/main/' ./temp/test-2.fif
+sed -i 's/recv_internal/recv_internal_deleted/' ./temp/test-2.fif
+sed -i 's/0 constant seq_no/3 constant seq_no/' ./temp/test-2.fif
+
 cd temp
 fift -s ./../dist/new-multisig.fif 0 6 4 test-run
 fift -s ./test-1.fif 0 6 4 test-run
