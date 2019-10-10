a39d558f829a304de9a018a9201a989bca355559
ftkvyn <ftkvyn@gmail.com>
2019-10-10 11:32:45 +0200

    Fixing the code

--- work/func/compile.sh
+++ work/func/compile.sh
@@ -1,3 +1,3 @@
 func -AP -O 0 -o result_2.fif stdlib.fc wallet-code.fc
-func -O 0 -o result_2.fif stdlib.fc wallet-code.fc -W test.boc
+func -O0 -o result_2.fif stdlib.fc wallet-code.fc -W test.boc
 fift -v 3 result_2.fif 
