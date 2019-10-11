4c9ef88fd65e02380a56ea08f9cf3c2bd857f6a2
ftkvyn <ftkvyn@gmail.com>
2019-10-11 20:30:21 +0200

    Helpers

--- work/dns-manual/build.sh
+++ work/dns-manual/build.sh
@@ -1,6 +1,6 @@
 rm ./dist/*.*
 rm ./temp/*.fif
-#func -AP -O0 -o ./dist/new-multisig-programm.fif ./lib/stdlib.fc ./src/multisig.fc
+func -AP -O0 -o ./dist/new-dns-manual-programm.fif ./lib/stdlib.fc ./src/helpers.fc ./src/dns-resolver.fc
 #cat ./dist/new-multisig-programm.fif ./src/new-multisig-wallet_script.fif > ./dist/new-multisig.fif
 #cp ./src/multisig-transaction.fif ./dist/multisig-transaction.fif
 
