a22ed6a760e8e99bd1b9cfb8e0bfcec34e7f1a0c
ftkvyn <ftkvyn@gmail.com>
2019-10-11 20:56:04 +0200

    Trying to work with strings

--- work/dns-manual/build.sh
+++ work/dns-manual/build.sh
@@ -4,11 +4,10 @@ func -AP -O0 -o ./dist/new-dns-manual-programm.fif ./lib/stdlib.fc ./src/helpers
 #cat ./dist/new-multisig-programm.fif ./src/new-multisig-wallet_script.fif > ./dist/new-multisig.fif
 #cp ./src/multisig-transaction.fif ./dist/multisig-transaction.fif
 
-#cat ./dist/new-multisig-programm.fif ./dist/multisig-transaction.fif ./test/test-storage.fif ./test/init-c7.fif ./test/test-3_script.fif > ./temp/test-3.fif
+cat ./test/init-c7.fif ./test/test-storage.fif ./dist/new-dns-manual-programm.fif ./test/test-2_script.fif > ./temp/test-2.fif
 #sed -i 's/0 constant seq_no/5 constant seq_no/' ./temp/test-3.fif
-#sed -i 's/#!\/usr\/bin\/env fift -s/constant code/' ./temp/test-3.fif
+sed -i 's/#!\/usr\/bin\/env fift -s/constant code/' ./temp/test-2.fif
 
 mkdir -p temp
 cd temp
-#fift -s ./../dist/new-multisig.fif 0 6 4 test-run
-#fift -s ./test-1.fif 0 6 4 test-run
+fift -s ./test-2.fif
