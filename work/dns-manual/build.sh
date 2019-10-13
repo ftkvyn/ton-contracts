421936c107c2f4051ce3f11ee4ab5cf77a67932d
ftkvyn <ftkvyn@gmail.com>
2019-10-13 20:36:38 +0200

    Fixing issues

--- work/dns-manual/build.sh
+++ work/dns-manual/build.sh
@@ -3,6 +3,7 @@ rm ./temp/*.fif
 func -AP -O0 -o ./dist/new-dns-manual-programm.fif ./lib/stdlib.fc ./src/dns-resolver.fc
 cat ./dist/new-dns-manual-programm.fif ./src/new-resolver_script.fif > ./dist/new-resolver.fif
 cp ./src/dns-resolver-key-update.fif ./dist/dns-resolver-key-update.fif
+cp ./src/update-dns-entry.fif ./dist/update-dns-entry.fif
 
 cat ./test/init-c7.fif ./test/test-storage.fif ./dist/new-dns-manual-programm.fif ./test/test-2_script.fif > ./temp/test-2.fif
 
@@ -16,9 +17,9 @@ cat ./test/init-c7.fif ./test/test-storage.fif ./dist/new-resolver.fif ./test/te
 
 mkdir -p temp
 cd temp
-#fift -s ./../dist/new-resolver.fif 0 test-run
-#fift -s ./test-2.fif
-#fift -s ./test-3.fif 0 test-run
-#fift -s ./test-4.fif 0 test-run
-#fift -s ./test-5.fif 0 test-run
+fift -s ./../dist/new-resolver.fif 0 test-run
+fift -s ./test-2.fif
+fift -s ./test-3.fif 0 test-run
+fift -s ./test-4.fif 0 test-run
+fift -s ./test-5.fif 0 test-run
 fift -s ./test-6.fif 0 test-run
