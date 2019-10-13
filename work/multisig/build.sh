335d7379ac1198e6bfa49d23e651f639aaed9b1c
ftkvyn <ftkvyn@gmail.com>
2019-10-13 21:15:40 +0200

    Fixing multisig

--- work/multisig/build.sh
+++ work/multisig/build.sh
@@ -10,19 +10,19 @@ cat ./dist/new-multisig-programm.fif ./src/new-multisig-wallet_script.fif ./test
 sed -i 's/0 constant seq_no/3 constant seq_no/' ./temp/test-2.fif
 
 cat ./dist/new-multisig-programm.fif ./dist/multisig-transaction.fif ./test/test-storage.fif ./test/init-c7.fif ./test/test-3_script.fif > ./temp/test-3.fif
-sed -i 's/0 constant seq_no/5 constant seq_no/' ./temp/test-3.fif
+sed -i 's/0 constant seq_no/1 constant seq_no/' ./temp/test-3.fif
 sed -i 's/#!\/usr\/bin\/env fift -s/constant code/' ./temp/test-3.fif
 
 cat ./dist/new-multisig-programm.fif ./dist/multisig-transaction.fif ./test/test-storage.fif ./test/init-c7.fif ./test/test-4_script.fif > ./temp/test-4.fif
-sed -i 's/0 constant seq_no/5 constant seq_no/' ./temp/test-4.fif
+sed -i 's/0 constant seq_no/1 constant seq_no/' ./temp/test-4.fif
 sed -i 's/#!\/usr\/bin\/env fift -s/constant code/' ./temp/test-4.fif
 
 cat ./dist/new-multisig-programm.fif ./dist/multisig-transaction.fif ./test/test-storage.fif ./test/init-c7.fif ./test/test-5_script.fif > ./temp/test-5.fif
-sed -i 's/0 constant seq_no/5 constant seq_no/' ./temp/test-5.fif
+sed -i 's/0 constant seq_no/1 constant seq_no/' ./temp/test-5.fif
 sed -i 's/#!\/usr\/bin\/env fift -s/constant code/' ./temp/test-5.fif
 
 cat ./dist/new-multisig-programm.fif ./dist/multisig-transaction.fif ./test/test-storage.fif ./test/init-c7.fif ./test/test-6_script.fif > ./temp/test-6.fif
-sed -i 's/0 constant seq_no/5 constant seq_no/' ./temp/test-6.fif
+sed -i 's/0 constant seq_no/1 constant seq_no/' ./temp/test-6.fif
 sed -i 's/#!\/usr\/bin\/env fift -s/constant code/' ./temp/test-6.fif
 
 mkdir -p temp
@@ -30,7 +30,7 @@ cd temp
 fift -s ./../dist/new-multisig.fif 0 6 4 test-run
 fift -s ./test-1.fif 0 6 4 test-run
 fift -s ./test-2.fif 0 6 4 test-run
-fift -s ./test-3.fif test-run 3 0QCyt4ltzak71h6XkyK4ePfZCzJQDSVUNuvZ3VE7hP_Q-GTE 5 1 hello
-fift -s ./test-4.fif test-run 3 0QCyt4ltzak71h6XkyK4ePfZCzJQDSVUNuvZ3VE7hP_Q-GTE 5 1 hello
-fift -s ./test-5.fif test-run 3 0QCyt4ltzak71h6XkyK4ePfZCzJQDSVUNuvZ3VE7hP_Q-GTE 5 1 hello
-fift -s ./test-6.fif test-run 3 0QCyt4ltzak71h6XkyK4ePfZCzJQDSVUNuvZ3VE7hP_Q-GTE 5 1 hello
+fift -s ./test-3.fif test-run 3 0QCyt4ltzak71h6XkyK4ePfZCzJQDSVUNuvZ3VE7hP_Q-GTE 1 1 hello
+fift -s ./test-4.fif test-run 3 0QCyt4ltzak71h6XkyK4ePfZCzJQDSVUNuvZ3VE7hP_Q-GTE 1 1 hello
+fift -s ./test-5.fif test-run 3 0QCyt4ltzak71h6XkyK4ePfZCzJQDSVUNuvZ3VE7hP_Q-GTE 1 1 hello
+fift -s ./test-6.fif test-run 3 0QCyt4ltzak71h6XkyK4ePfZCzJQDSVUNuvZ3VE7hP_Q-GTE 1 1 hello
