d254302c2b028b8d5c4091705d2857d9b11c2466
ftkvyn <ftkvyn@gmail.com>
2019-10-13 15:38:49 +0200

    Update key script

--- work/dns-manual/build.sh
+++ work/dns-manual/build.sh
@@ -2,7 +2,7 @@ rm ./dist/*.*
 rm ./temp/*.fif
 func -AP -O0 -o ./dist/new-dns-manual-programm.fif ./lib/stdlib.fc ./src/dns-resolver.fc
 cat ./dist/new-dns-manual-programm.fif ./src/new-resolver_script.fif > ./dist/new-resolver.fif
-#cp ./src/multisig-transaction.fif ./dist/multisig-transaction.fif
+cp ./src/dns-resolver-key-update.fif ./dist/dns-resolver-key-update.fif
 
 cat ./test/init-c7.fif ./test/test-storage.fif ./dist/new-dns-manual-programm.fif ./test/test-2_script.fif > ./temp/test-2.fif
 sed -i 's/#!\/usr\/bin\/env fift -s/constant code/' ./temp/test-2.fif
