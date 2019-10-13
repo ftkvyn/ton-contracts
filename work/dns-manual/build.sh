993b02a97a7ee6f700989c48723bc6cf279cb6e4
ftkvyn <ftkvyn@gmail.com>
2019-10-13 13:57:31 +0200

    Code of the resolver

--- work/dns-manual/build.sh
+++ work/dns-manual/build.sh
@@ -1,6 +1,6 @@
 rm ./dist/*.*
 rm ./temp/*.fif
-func -AP -O0 -o ./dist/new-dns-manual-programm.fif ./lib/stdlib.fc ./src/helpers.fc ./src/dns-resolver.fc
+func -AP -O0 -o ./dist/new-dns-manual-programm.fif ./lib/stdlib.fc ./src/dns-resolver.fc
 #cat ./dist/new-multisig-programm.fif ./src/new-multisig-wallet_script.fif > ./dist/new-multisig.fif
 #cp ./src/multisig-transaction.fif ./dist/multisig-transaction.fif
 
