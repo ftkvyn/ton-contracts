55f705f3780eec0346f08e4c88988d9ac070a8c9
ftkvyn <ftkvyn@gmail.com>
2019-10-13 17:42:47 +0200

    Working updates

--- work/dns-manual/build.sh
+++ work/dns-manual/build.sh
@@ -5,21 +5,20 @@ cat ./dist/new-dns-manual-programm.fif ./src/new-resolver_script.fif > ./dist/ne
 cp ./src/dns-resolver-key-update.fif ./dist/dns-resolver-key-update.fif
 
 cat ./test/init-c7.fif ./test/test-storage.fif ./dist/new-dns-manual-programm.fif ./test/test-2_script.fif > ./temp/test-2.fif
-sed -i 's/#!\/usr\/bin\/env fift -s/constant code/' ./temp/test-2.fif
 
 cat ./test/init-c7.fif ./dist/new-resolver.fif ./test/test-3_script.fif > ./temp/test-3.fif
-sed -i 's/#!\/usr\/bin\/env fift -s/constant code/' ./temp/test-3.fif
 
 cat ./test/init-c7.fif ./dist/new-resolver.fif ./test/test-4_script.fif > ./temp/test-4.fif
-sed -i 's/#!\/usr\/bin\/env fift -s/constant code/' ./temp/test-4.fif
 
 cat ./test/init-c7.fif ./test/test-storage.fif ./dist/new-dns-manual-programm.fif ./test/test-5_script.fif > ./temp/test-5.fif
-sed -i 's/#!\/usr\/bin\/env fift -s/constant code/' ./temp/test-5.fif
+
+cat ./test/init-c7.fif ./test/test-storage.fif ./dist/new-resolver.fif ./test/test-6_script.fif > ./temp/test-6.fif
 
 mkdir -p temp
 cd temp
-fift -s ./../dist/new-resolver.fif 0 test-run
-fift -s ./test-2.fif
+#fift -s ./../dist/new-resolver.fif 0 test-run
+#fift -s ./test-2.fif
 #fift -s ./test-3.fif 0 test-run
 #fift -s ./test-4.fif 0 test-run
-fift -s ./test-5.fif 0 test-run
+#fift -s ./test-5.fif 0 test-run
+fift -s ./test-6.fif 0 test-run
