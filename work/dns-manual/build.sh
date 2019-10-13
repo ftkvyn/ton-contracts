22140516b5a8e134d0cfa80b80ee99f249b391e4
ftkvyn <ftkvyn@gmail.com>
2019-10-13 14:33:06 +0200

    Working update of keys

--- work/dns-manual/build.sh
+++ work/dns-manual/build.sh
@@ -10,9 +10,12 @@ sed -i 's/#!\/usr\/bin\/env fift -s/constant code/' ./temp/test-2.fif
 cat ./test/init-c7.fif ./dist/new-resolver.fif ./test/test-3_script.fif > ./temp/test-3.fif
 sed -i 's/#!\/usr\/bin\/env fift -s/constant code/' ./temp/test-3.fif
 
+cat ./test/init-c7.fif ./dist/new-resolver.fif ./test/test-4_script.fif > ./temp/test-4.fif
+sed -i 's/#!\/usr\/bin\/env fift -s/constant code/' ./temp/test-4.fif
 
 mkdir -p temp
 cd temp
 fift -s ./../dist/new-resolver.fif 0 test-run
 #fift -s ./test-2.fif
-fift -s ./test-3.fif 0 test-run
+#fift -s ./test-3.fif 0 test-run
+fift -s ./test-4.fif 0 test-run
