e3e1bdbe34725acd926b8ac45e86fa232d8bf106
ftkvyn <ftkvyn@gmail.com>
2019-10-14 21:15:19 +0200

    Some code for auto dns resolver

--- work/dns-auto/build.sh
+++ work/dns-auto/build.sh
@@ -1,23 +1,8 @@
 rm ./dist/*.*
 rm ./temp/*.fif
-func -AP -O0 -o ./dist/new-dns-manual-programm.fif ./lib/stdlib.fc ./src/dns-resolver.fc
-cat ./dist/new-dns-manual-programm.fif ./src/new-resolver_script.fif > ./dist/new-resolver.fif
-
-cat ./test/init-c7.fif ./test/test-storage.fif ./dist/new-dns-manual-programm.fif ./test/test-2_script.fif > ./temp/test-2.fif
-
-cat ./test/init-c7.fif ./dist/new-resolver.fif ./test/test-3_script.fif > ./temp/test-3.fif
-
-cat ./test/init-c7.fif ./dist/new-resolver.fif ./test/test-4_script.fif > ./temp/test-4.fif
-
-cat ./test/init-c7.fif ./test/test-storage.fif ./dist/new-dns-manual-programm.fif ./test/test-5_script.fif > ./temp/test-5.fif
-
-cat ./test/init-c7.fif ./test/test-storage.fif ./dist/new-resolver.fif ./test/test-6_script.fif > ./temp/test-6.fif
+func -AP -O0 -o ./dist/new-dns-auto-programm.fif ./lib/stdlib.fc ./src/dns-resolver.fc
+cat ./dist/new-dns-auto-programm.fif ./src/new-resolver_script.fif > ./dist/new-resolver.fif
 
 mkdir -p temp
 cd temp
-fift -s ./../dist/new-resolver.fif 0 test-run
-fift -s ./test-2.fif
-fift -s ./test-3.fif 0 test-run
-fift -s ./test-4.fif 0 test-run
-fift -s ./test-5.fif 0 test-run
-fift -s ./test-6.fif 0 test-run
+fift -s ./../dist/new-resolver.fif 0 1 0.001 0.000001 test-run
